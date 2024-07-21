8d1f13361ef3cf0edd913b89090ed0909f115dbd
Allen Wang <allen.houze.wang@gmail.com>
2024-07-20 22:43:55 -0700

    update message colors (#28)
    * update message colors
    
    * dark green
    
    * newline
    
    * fix merge conflict
    
    * more merge conflicts
    
    * merge conflict yet again
    
    * fmt
    
    ---------
    
    Co-authored-by: Allen Wang <allen@Allens-MacBook-Pro.local>

--- src/haipera/nb.py
+++ src/haipera/nb.py
@@ -1,10 +1,11 @@
 import nbformat
 from nbconvert import PythonExporter
 
-__all__ = ['convert_ipynb_to_py']
+__all__ = ["convert_ipynb_to_py"]
+
 
 def convert_ipynb_to_py(ipynb_file: str) -> str:
-    with open(ipynb_file, 'r', encoding='utf-8') as nb_file:
+    with open(ipynb_file, "r", encoding="utf-8") as nb_file:
         nb_contents = nbformat.read(nb_file, as_version=4)
     exporter = PythonExporter()
     py_contents, _ = exporter.from_notebook_node(nb_contents)
